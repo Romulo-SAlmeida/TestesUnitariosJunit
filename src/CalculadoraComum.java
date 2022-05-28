9a31d2c4a4e93017f1174ef6d28a9568e3d150ec
Cauã Pablo <79022173+Caua-Pablo@users.noreply.github.com>
2022-05-27 21:03:15 -0300

    Implementando CalculadoraComum

---
+++ src/CalculadoraComum.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CalculadoraComum {
+
+    Scanner scan = new Scanner(System.in);
+    private int adicao,subtracao, divisao, mutiplicacao;
+
+    public int getAdicao() {
+        return adicao;
+    }
+
+    public void setAdicao(int adicao) {
+        this.adicao = adicao;
+    }
+
+
+    public int getSubtracao() {
+
+        return subtracao;
+    }
+
+    public void setSubtracao(int subtracao) {
+        this.subtracao = subtracao;
+    }
+
+    public int getDivisao() {
+        return divisao;
+    }
+
+    public void setDivisao(int divisao) {
+        this.divisao = divisao;
+    }
+
+    public int getMutiplicacao() {
+        return mutiplicacao;
+    }
+
+    public void setMutiplicacao(int mutiplicacao) {
+        this.mutiplicacao = mutiplicacao;
+    }
+}
